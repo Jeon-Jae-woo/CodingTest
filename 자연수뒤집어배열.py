e41e56236bad3e0d95dfcb8413e0541c1fc47b86
Jaewoo <jaewoo683@gmail.com>
2020-06-10 16:41:02 +0900

    add Problem

--- 자연수뒤집어배열.py
+++ 자연수뒤집어배열.py
@@ -1,9 +1,9 @@
-n = 12345 
+n = 12345
 string = str(n)
 answer = []
 for i in range(0,len(string)):
     answer.append(int(string[i]))
 
-print(answer)
 answer.reverse()
 print(answer)
+#return answer
